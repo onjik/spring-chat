901639fe16746efbae683aa764785d9e80c28da8
Onji Kim <xodks672@gmail.com>
2023-03-29 14:42:22 +0900

    [REFACTOR] refactoring
    refactoring

--- src/test/java/com/oj/springchat/test/util/MemberTestUtilTest.java
+++ src/test/java/com/oj/springchat/test/util/MemberTestUtilTest.java
@@ -14,6 +14,7 @@ class MemberTestUtilTest {
         assertEquals(signUpRequest.getEmail(),MemberTestUtil.testEmail);
         assertEquals(signUpRequest.getPassword(),MemberTestUtil.testPassword);
         assertEquals(signUpRequest.getNickname(),MemberTestUtil.testNickname);
+        assertEquals("a","b"); //git hub action test를 위해 일부러 틀린 테스트 작성
     }
 
 }
